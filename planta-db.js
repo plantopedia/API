700ee0137825df6a37188d99b9569aa5fe6d633f
Kirey Cazkdnsky <186427339+kireiygo@users.noreply.github.com>
2025-02-18 13:44:19 -0300

    Update planta-db.js

--- planta-db.js
+++ planta-db.js
@@ -1,35 +1,24 @@
 window.plantIndex = {
-"Pitangueira": "468865.js",
-"Pitangueira Comum": "468865.js",
-
-/* Test Files */
-"Pitangueira Roxa": "468865.js",
-"Pitangueira Verde": "468865.js",
-"Pitangueira Azul": "468865.js",
-"Pitangueira Amarelo": "468865.js",
-"Pitangueira Marrom": "468865.js",
-"Pitangueira Rosa": "468865.js",
-
-"Alta Pitangea": "468865.js",
-"Eugenia Uniflora": "468865.js",
-"Cerejeira do Rio Grande": "468865.js",
-"Pitangea": "468865.js",
-"Pitanga": "468865.js"
+    "Pitangueira": "468865.js",
+    "Pitangueira Comum": "468865.js",
+    "Alta Pitangea": "468865.js",
+    "Eugenia Uniflora": "468865.js",
+    "Cerejeira do Rio Grande": "468865.js",
+    "Pitangea": "468865.js",
+    "Pitanga": "468865.js"
 };
 
 function normalizeText(text) {
-return text
-.normalize("NFD") 
-.replace(/[\u0300-\u036f]/g, "") 
-.toLowerCase()
-.replace(/\s+/g, "-");
-
+    return text
+        .normalize("NFD") 
+        .replace(/[\u0300-\u036f]/g, "") 
+        .toLowerCase()
+        .replace(/\s+/g, "-");
 }
 
 window.normalizedIndex = {};
 for (let key in window.plantIndex) {
-window.normalizedIndex[normalizeText(key)] = window.plantIndex[key];
-
+    window.normalizedIndex[normalizeText(key)] = window.plantIndex[key];
 }
 
 function buscarPlantas(nome) {
